6835b0daf4fa9c6114ce23bcdf045469d49dfd29
Lakshan Madushanka <epmadushanka@gmail.com>
2022-06-13 14:36:55 +0530

    Fix some minor issues

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -2,17 +2,14 @@
 
 namespace Database\Seeders;
 
-<<<<<<< HEAD
 use App\Domains\Event\Model\Event;
-use App\Domains\Event\Services\EventService;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-=======
->>>>>>> 0e4811deb497ae1bcde168ded676a9e4b465aa63
 use Illuminate\Database\Seeder;
 
 class EventSeeder extends Seeder
 {
     use WithoutModelEvents;
+
     /**
      * Run the database seeds.
      *
